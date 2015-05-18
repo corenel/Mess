065d3c4076596eeb29a27621382d9ebd973df743
corenel <xxdsox@gmail.com>
2015-05-19 01:38:08 +0800

    Add ex11-13 of LPTHW

---
+++ Learn Python The Hard Way/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv  # argv stands for "argument variable"
+
+print "This script is called:", script
+print "The first variable:", first
+print "The second is:", second
+print "And the last is:", third
